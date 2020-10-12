58af36a324e016dbbcd68fbfd588e96bac15930c
gauthier <gauthierbouyjou@aol.com>
2020-10-12 15:25:50 +0200

    vulkan helloWorld

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
+//#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan.hpp>
+//#include <vulkan/vk_layer.h>
+//#include <vulkan/vulkan_core.h>
 
-using namespace std;
+int main() {
+    glfwInit();
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+//    vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+//    vk::enumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+//    glm::mat4 matrix;
+//    glm::vec4 vec;
+//    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
